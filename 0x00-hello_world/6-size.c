5835daa98f9ab62419fe84dc9adea6f1c91d1f32
ILYAS-SE <chilyasse2@gmail.com>
2023-09-10 16:30:22 -0700

    /**

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the size of various data types on the computer.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
