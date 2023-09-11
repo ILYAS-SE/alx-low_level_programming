913f327608048444d28929db764cdd5a9b75d782
ILYAS-SE <chilyasse2@gmail.com>
2023-09-11 13:08:41 -0700

    #include <stdio.h>

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,17 +7,21 @@
  */
 int main(void)
 {
-int digit = '0';
- while (digit <= '9')
-}    
-putchar(digit);
-if (digit != '9')
-}        
-putchar(',');
-putchar(' ');
+    int digit;
+
+    for (digit = 0; digit <= 9; digit++)
+    {
+        putchar(digit + '0');
+
+        if (digit != 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+
+    putchar('\n');
+
+    return (0);
 }
-digit++;
-}
-putchar('\n');
-return (0);
 }
