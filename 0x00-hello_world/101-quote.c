d55c5ca5feea033d513dd76b35d0ba1a34e0cbe7
ILYAS-SE <chilyasse2@gmail.com>
2023-09-10 17:19:43 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,7 @@
  */
 int main(void)
 {
-char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, message, sizeof(message) - 1);
-return (1);
+write(2, "and that piece of art is useful\"", 32);   
+write(2, " - Dora Korpar, 2015-10-19\n", 26);
+ return (1);
 }
-
-
