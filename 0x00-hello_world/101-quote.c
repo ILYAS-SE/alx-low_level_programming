1f178d9763023517e8c71bfc65997fe2029574e2
ILYAS-SE <chilyasse2@gmail.com>
2023-09-10 17:13:27 -0700

    #include <unistd.h>

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
@@ -6,7 +7,9 @@
  */
 int main(void)
 {
-write(2,"and that piece of art is useful\" -Dora Korpar,2015-10-19\n",59);
+char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, sizeof(message) - 1);
 return (1);
 }
+
 
