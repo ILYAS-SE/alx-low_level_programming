351651ad1311d300afbbb6cea060fce63f048f02
ILYAS-SE <chilyasse2@gmail.com>
2023-09-11 09:49:35 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -12,7 +12,7 @@ putchar('0' + 1); /* Print '1' by adding 1 to '0' character */
 putchar('0' + 2); /* Print '2' by adding 2 to '0' character */
 putchar('0' + 3); /* Print '3' by adding 3 to '0' character */
 putchar('0' + 4); /* Print '4' by adding 4 to '0' character */
-putcher('0' + 5); /* Print '5' by adding 5 to '0' character */
+putchar('0' + 5); /* Print '5' by adding 5 to '0' character */
 putchar('0' + 6); /* Print '6' by adding 6 to '0' character */
 putchar('0' + 7); /* Print '7' by adding 7 to '0' character */
 putchar('0' + 8); /* Print '8' by adding 8 to '0' character */
