1be26aaffec5cb1315b4bb26b8ab9f84a309679c
ILYAS-SE <chilyasse2@gmail.com>
2023-09-13 14:57:45 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+void print_alphabet(void);
+void _putchar(char a);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int print_last_digit(int a);
+int _abs(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void multiple();
+void print_times_table(int n);
+#endif
