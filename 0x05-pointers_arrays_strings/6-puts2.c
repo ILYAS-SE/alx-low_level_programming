3aa566f7fa8936dbc830942a547f4486e2233332
ILYAS-SE <chilyasse2@gmail.com>
2023-09-18 15:56:15 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,27 +1,17 @@
 /**
-* print_diagonal - function that draws a diagonal line on the terminal.
+* print_line - function that prints 10 times the numbers, from 0 to 14.
 * The line should end with a \n.
 * @n: para checked
 * Return: void.
 */
 #include "main.h"
-void print_diagonal(int n)
+void print_line(int n)
 {
-int i, j;
-if (n <= 0)
+int i = 0;
+while (i < n)
 {
+_putchar(95);
+i++;
+}
 _putchar('\n');
-}
-else
-{
-for (i = 0; i < n; i++)
-{
-for (j = 1; j <= i; j++)
-{
-_putchar(' ');
-}
-_putchar(92);
-_putchar('\n');
-}
-}
 }
