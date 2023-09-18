67eb477a85b5423aa466d98095fd610437e52914
ILYAS-SE <chilyasse2@gmail.com>
2023-09-18 13:11:38 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,4 @@
-#include "main.h"  // Include the necessary header file with _putchar declaration.
+#include "main.h"
 
 /**
  * print_rev - Prints a string in reverse followed by a new line.
@@ -13,7 +13,8 @@ while (s[length] != '\0')
 length++;
 }
 /* Print the string in reverse */
-for (int i = length - 1; i >= 0; i--)
+int i;
+for (i = length - 1; i >= 0; i--)
 {
 _putchar(s[i]);
 }
