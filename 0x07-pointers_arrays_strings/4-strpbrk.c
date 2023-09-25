f59e0252268f3aa72bfabff3ddaf75105336c326
ILYAS-SE <chilyasse2@gmail.com>
2023-09-25 13:29:13 -0700

    *

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,14 +10,17 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-int i, j;
-for (i = 0; s[i]; i++)
-{
-for (j = 0; accept[j]; j++)
-{
-if (s[i] == accept[j])
-return (s + i);
+    int i, j;
+
+    for (i = 0; s[i]; i++)
+    {
+        for (j = 0; accept[j]; j++)
+        {
+            if (s[i] == accept[j])
+                return (s + i);
+        }
+    }
+
+    return (NULL);
 }
-}
-return (NULL);
-}
+
