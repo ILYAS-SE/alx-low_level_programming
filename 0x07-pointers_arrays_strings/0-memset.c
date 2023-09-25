17cc367c06182ef804658f3c1bfb755cff639a68
ILYAS-SE <chilyasse2@gmail.com>
2023-09-25 13:25:48 -0700

    *

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_memset -   fills memory with a constant byte .
+ *@s: pointer block of memory to fill .
+ *@b: constant byte .
+ *@n: bytes of the memory .
+ *Return: pointer s .
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
