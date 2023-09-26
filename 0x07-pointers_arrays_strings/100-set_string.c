4cfcdf91ac48ab0b2cf5e458349d74311e2c15bd
ILYAS-SE <chilyasse2@gmail.com>
2023-09-26 02:25:03 -0700

    *

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -8,12 +8,12 @@
  */
 void print_diagsums(int *a, int size)
 {
-int i;
-int sum1 = 0, sum2 = 0;
+ int i;
+ int sum1 = 0, sum2 = 0;
 for (i = 0; i < size; i++)
-{
-sum1 += a[i * size + i]; // Sum of the main diagonal
-sum2 += a[i * size + (size - 1 - i)]; // Sum of the other diagonal
-}
+     {
+  sum1 += a[i * size + i]; // Sum of the main diagonal
+  sum2 += a[i * size + (size - 1 - i)]; // Sum of the other diagonal
+     }
 printf("%d, %d\n", sum1, sum2);
-}
+     }
