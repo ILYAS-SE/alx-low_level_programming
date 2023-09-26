4f499a4e08c4c6e5eab4a62bb46be13144f72773
ILYAS-SE <chilyasse2@gmail.com>
2023-09-26 02:28:24 -0700

    *

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,19 +1,29 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * print_diagsums - Sum of the two diagonals of a square matrix of int.
- * @a: Pointer to the array representing the matrix.
- * @size: Size of the square matrix.
+ * print_diagsums - sum of the two diagonals of a square matrix of int
+ * @a: pointer to array
+ * @size: size of array.
+ * Return: void.
  */
 void print_diagsums(int *a, int size)
 {
- int i;
- int sum1 = 0, sum2 = 0;
+int i, j;
+int sum1 = 0, sum2 = 0;
+
 for (i = 0; i < size; i++)
-     {
-  sum1 += a[i * size + i]; // Sum of the main diagonal
-  sum2 += a[i * size + (size - 1 - i)]; // Sum of the other diagonal
-     }
+{
+for (j = 0; j < size; j++)
+{
+if (i == j)
+{
+sum1 += a[i * size + j];
+}
+if (i + j == size - 1)
+{
+sum2 += a[i * size + j];
+}
+}
+}
 printf("%d, %d\n", sum1, sum2);
-     }
+}
