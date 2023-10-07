eec2027a31de76e66bdc846ad8f5f61621ce6fed
ILYAS-SE <chilyasse2@gmail.com>
2023-10-07 13:03:05 -0700

    *

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,15 +13,16 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-	for(j = 0; argv[i][j] != '\0'; j++)
-	{
-		if (!isdigit(argv[i][j]))												{
-																	       printf("Error\n");
-		       return (1);
-		}												
+	      for(j = 0; argv[i][j] != '\0'; j++)
+	      {
+		      if (!isdigit(argv[i][j]))
+		      {
+			      printf("Error\n");
+			      return (1);
+		      }
+	      }
+	      add += atoi(argv[i]);
 	}
-	add += atoi(argv[i]);
-        }
 	printf("%d\n", add);
 	return (0);
 }
