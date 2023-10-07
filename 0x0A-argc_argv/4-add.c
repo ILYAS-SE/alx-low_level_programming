dcfeba19ce67cf4b1cd3018f74b4df4ec29f7ce5
ILYAS-SE <chilyasse2@gmail.com>
2023-10-07 15:09:53 -0700

    *

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,10 +12,11 @@
 int isNum(char num[])
 {
 	int i, l = strlen(num);
+
         for (i = 0; i < l; i++)
 	{
-        if (!isdigit(num[i]))
-	return (1);
+                 if (!isdigit(num[i]))
+	                 return (1);
 	}
 	return (0);
 }
@@ -30,28 +31,28 @@ int isNum(char num[])
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
-        {
+{
 	int i, sum;
 	if (argc == 1)
 	{
-	printf("0\n");
+	        printf("0\n");
         }
         else
 	{
-	sum = 0;
-	for (i = 1; i < argc; i++)
-        {
-        if (isNum(argv[i]) == 0)
-        {
-	sum += atoi(argv[i]);
-        }
-        else
-        {
-	printf("Error\n");
-        return (1);
-	}
-	}
-	printf("%d\n", sum);
+	        sum = 0;
+	        for (i = 1; i < argc; i++)
+                {
+                        if (isNum(argv[i]) == 0)
+                        {
+	                      sum += atoi(argv[i]);
+                        }
+                        else
+                        {
+	                      printf("Error\n");
+                              return (1);
+	                }
+	       }     
+	       printf("%d\n", sum);
 	}
 	return (0);
 }
