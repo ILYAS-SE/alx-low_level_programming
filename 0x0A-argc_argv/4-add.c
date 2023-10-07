651e7363f7a67f9c583006bf3370d22925ed01c2
ILYAS-SE <chilyasse2@gmail.com>
2023-10-07 15:17:20 -0700

    *

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,17 +11,14 @@
  */ 
 int isNum(char num[])
 {
-	int i, l = strlen(num);
-
-        for (i = 0; i < l; i++)
-	{
-                 if (!isdigit(num[i]))
-	                 return (1);
-	}
-	return (0);
+int i, l = strlen(num);
+for (i = 0; i < l; i++)
+{
+if (!isdigit(num[i]))
+return (1);
 }
-
-
+return (0);
+}
 /**
  * main - a program that adds positive numbers
  *
@@ -32,27 +29,27 @@ int isNum(char num[])
  */
 int main(int argc, char *argv[])
 {
-	int i, sum;
-	if (argc == 1)
-	{
-	        printf("0\n");
-        }
-        else
-	{
-	        sum = 0;
-	        for (i = 1; i < argc; i++)
-                {
-                        if (isNum(argv[i]) == 0)
-                        {
-	                      sum += atoi(argv[i]);
-                        }
-                        else
-                        {
-	                      printf("Error\n");
-                              return (1);
-	                }
-	       }     
-	       printf("%d\n", sum);
-	}
-	return (0);
+int i, sum;
+if (argc == 1)
+{
+printf("0\n");
+}
+else
+{
+sum =0;
+for (i = 1; i < argc; i++)
+{
+if (isNum(argv[i]) == 0)
+{                     
+sum += atoi(argv[i]);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}     
+printf("%d\n", sum);
+}
+return (0);
 }
