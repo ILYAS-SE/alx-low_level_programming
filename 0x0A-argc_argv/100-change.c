9a715e7684912765ccb74966f46942902ab62287
ILYAS-SE <chilyasse2@gmail.com>
2023-10-07 12:56:57 -0700

    *

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,49 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - prints the min number of coins to make change
- * for an amount of money
- * @argc: argument count
- * @argv: arguments
- * Return: 0
- *       */
-int main(int argc, char **argv)
+ * main - prints the minimum number of coins to make change for an amount.
+ * of money.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
 {
-int total, count;
-unsigned int i;
-char *p;
-int cents[] = {25, 10, 5, 2};
+	int cents, Coins = 0;
 
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
-}
+        if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+	        return (1);
+	}
 
-total = strtol(argv[1], &p, 10);
-count = 0;
+	cents = atoi(argv[1]);
 
-if (!*p)
-{
-while (total > 1)
-{
-for (i = 0; i < sizeof(cents[i]); i++)
-{
-if (total >= cents[i])
-{
-count += total / cents[i]																							   total = total % cents[i];
-}						
-}
-}
-if (total == 1)
-count++;
-}
-else
-{
-printf("Error\n");
-return (1);
-}
-printf("%d\n", count);
-return (0);
+	while (cents > 0)
+	{
+	       if (cents >= 25)
+			cents -= 25;
+	       else if (cents >= 10)
+			cents -= 10;
+	       else if (cents >= 5)														cents -= 5;
+	       else if (cents >= 2)														cents -= 2;
+	       else if (cents >= 1)
+		        cents -= 1;												       Coins += 1;
+																}
+	printf("%d\n", Coins);
+	return (0);
 }
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
-#include "main.h"
+
+#include <stdio.h>
 /**
-  main - Entry point.
- *
- * @argc: size of arguments
- * @argv: arguments array
- *
- * Return: 0.
+ * main - prints all arguments it receives.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-int i;
+	int i;
 
-for (i = 0; i < argc; i++)
-{
-printf("%s\n", argv[i]);
-}
-
-return (0);
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
 }
