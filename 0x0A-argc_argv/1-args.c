e6c13fba381003df82cbafd74e26f0f0cea3c615
ILYAS-SE <chilyasse2@gmail.com>
2023-10-07 07:07:21 -0700

    *

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point.
+ *
+ * @argc: size of arguments
+ * @argv: arguments array
+ * 
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%d\n", argc - 1);
+return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Entry point.
+ *
+ * @argc: size of arguments
+ * @argv: arguments array
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+int res = 0;
+if (argc <= 2)
+{
+printf("Error\n");
+}
+
+if (argc >= 3)
+{
+res = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", res);
+}
+
+return (0);
+}
