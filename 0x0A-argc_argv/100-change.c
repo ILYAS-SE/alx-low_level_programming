b42208738924d6543e9068b282955c432012dab5
ILYAS-SE <chilyasse2@gmail.com>
2023-10-07 13:09:20 -0700

    *

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -25,11 +25,14 @@ int main(int argc, char *argv[])
 			cents -= 25;
 	       else if (cents >= 10)
 			cents -= 10;
-	       else if (cents >= 5)														cents -= 5;
-	       else if (cents >= 2)														cents -= 2;
+	       else if (cents >= 5)
+		       cents -= 5;
+	       else if (cents >= 2)
+		       cents -= 2;
 	       else if (cents >= 1)
-		        cents -= 1;												       Coins += 1;
-																}
+		        cents -= 1;
+	       Coins += 1;
+	}
 	printf("%d\n", Coins);
 	return (0);
 }
