25f0c2ef5f2d64993ecfc50b76f3bde9c8d9617e
ILYAS-SE <chilyasse2@gmail.com>
2023-10-24 12:08:53 -0700

    *

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,25 +1,19 @@
 #include "lists.h"
+
 /**
- * sum_listint - thsi function adds the values stores in the list
- * @head: the head of list
- * Description: this function adds the values)?
- * section header: the header of this function is lists.h)*
- * Return: return the final result of the sum
+ * sum_listint - returns the sum of all the data stored on linked list
+ * @head: pointer to the first elemnt of the list
+ *
+ * Return: sum of the stored data
  */
 int sum_listint(listint_t *head)
 {
-	listint_t *actual;
-	int result;
+	int sum = 0;
 
-	result = 0;
-
-	if (head == NULL)
-		return (0);
-	actual = head;
-	while (actual != '\0')
+	while (head)
 	{
-		result = result + actual->n;
-		actual = actual->next;
+		sum += head->n;
+		head = head->next;
 	}
-	return (result);
+	return (sum);
 }
