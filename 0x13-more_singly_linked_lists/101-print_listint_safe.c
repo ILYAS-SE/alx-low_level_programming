d448eb1487c146f33e38bbb4a48391dc9828c0cf
ILYAS-SE <chilyasse2@gmail.com>
2023-10-24 12:14:17 -0700

    *

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,34 +1,61 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
- * print_listint_safe - thsi function print a list in safe mode
- * @head: the head of list
- * Description: this function print a string in a safe mode
- * section header: the header of this function is lists.h)*
- * Return: the size of the list
+ * _r - reallocates memory for an array of pointers
+ * to the nodes in a linked list
+ * @list: the old list to append
+ * @size: size of the new list (always one more than the old list)
+ * @new: new node to add to the list
+ *
+ * Return: pointer to the new list
+ */
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
+{
+	const listint_t **newlist;
+	size_t i;
+
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+		newlist[i] = list[i];
+	newlist[i] = new;
+	free(list);
+	return (newlist);
+}
+
+/**
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: pointer to the start of the list
+ *
+ * Return: the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	int difference, i, number;
-	void *address;
-
-	i = 0;
+	size_t i, num = 0;
+	const listint_t **list = NULL;
 
 	while (head != NULL)
 	{
-		difference = head - head->next;
-		i++;
-		number = head->n;
-		printf("[%p] %i\n", (void *)head, number);
-		if (difference > 0)
-			head = head->next;
-		else
+		for (i = 0; i < num; i++)
 		{
-			address = head->next;
-			number = head->next->n;
-			printf("-> [%p] %i\n", address, number);
-			break;
+			if (head == list[i])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(list);
+				return (num);
+			}
 		}
-
+		num++;
+		list = _r(list, num, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
-	return (i);
+	free(list);
+	return (num);
 }
